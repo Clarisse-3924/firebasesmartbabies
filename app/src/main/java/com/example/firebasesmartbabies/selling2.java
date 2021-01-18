1883d2230876671130b94d13f4ab48e0356b1f9b
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2021-01-18 10:10:01 +0200

    adding activity called selling 2

---
+++ app/src/main/java/com/example/firebasesmartbabies/selling2.java
@@ -0,0 +1,14 @@
+package com.example.firebasesmartbabies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class selling2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selling2);
+    }
+}
