ad63b5e1ddaad64d874a955429fc64afadc2340c
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2021-01-18 16:46:20 +0200

    adding search menu

--- app/src/main/java/com/example/smartbabies/MainActivity.java
+++ app/src/main/java/com/example/smartbabies/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.content.DialogInterface;
@@ -13,45 +15,110 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ImageAdapter1.onItemclickListener{
     private TextView Name;
-    private Button store;
     DrawerLayout drawerLayout;
-List<listview>list;
-ListView list_item;
+
+    private RecyclerView mRecyclerView;
+    private ImageAdapter1 mAdapter;
+
+    private DatabaseReference mDatabaseRef;
+    private FirebaseStorage mStorage;
+    private ValueEventListener mDRListener;
+    private List<upload> muploads;
+
+//List<listview>list;
+//ListView list_item;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Name = (TextView) findViewById(R.id.name);
-        store=findViewById(R.id.store);
 
-        store.setOnClickListener(new View.OnClickListener() {
+        mRecyclerView=findViewById(R.id.myrecycleview1);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        muploads=new ArrayList<>();
+        mAdapter = new ImageAdapter1(MainActivity.this,muploads);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.setOnItemClickListener(MainActivity.this);
+
+        mStorage =FirebaseStorage.getInstance();
+        mDatabaseRef= FirebaseDatabase.getInstance().getReference("uploads");
+
+        mDRListener= mDatabaseRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), selling2.class));
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                muploads.clear();
+                for(DataSnapshot postSnapshot: dataSnapshot.getChildren())
+                {
+                    upload Upload=postSnapshot.getValue(upload.class);
+                    Upload.setKey(postSnapshot.getKey());
+                    muploads.add(Upload);
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this,databaseError.getMessage(),Toast.LENGTH_SHORT).show();
+
+
             }
         });
+
         drawerLayout = findViewById(R.id.drawer_layout);
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
         Name.setText("WELCOME TO OUR STORE: " + name);
-        list=new ArrayList<>();
-        list.add(new listview(R.drawable.baby1,"Dress","5000 RWF", "Add To Cart"));
-        list.add(new listview(R.drawable.baby2,"Dress","10000 RWF", "Add To Cart"));
-        list.add(new listview(R.drawable.baby3,"Dress","6000 RWF", "Add To Cart"));
-        list.add(new listview(R.drawable.baby4,"Trousers","15000 RWF", "Add To Cart"));
-        list.add(new listview(R.drawable.baby5,"Trousers","7000 RWF", "Add To Cart"));
-        list.add(new listview(R.drawable.baby6,"Shirt","5000 RWF", "Add To Cart"));
-        list.add(new listview(R.drawable.baby7,"T-shirt","3000 RWF", "Add To Cart"));
+//        list=new ArrayList<>();
+//        list.add(new listview(R.drawable.baby1,"Dress","5000 RWF", "Add To Cart"));
+//        list.add(new listview(R.drawable.baby2,"Dress","10000 RWF", "Add To Cart"));
+//        list.add(new listview(R.drawable.baby3,"Dress","6000 RWF", "Add To Cart"));
+//        list.add(new listview(R.drawable.baby4,"Trousers","15000 RWF", "Add To Cart"));
+//        list.add(new listview(R.drawable.baby5,"Trousers","7000 RWF", "Add To Cart"));
+//        list.add(new listview(R.drawable.baby6,"Shirt","5000 RWF", "Add To Cart"));
+//        list.add(new listview(R.drawable.baby7,"T-shirt","3000 RWF", "Add To Cart"));
+//
+//        list_item=findViewById(R.id.list_item);
+//        mylistadapter adapter= new mylistadapter(this,R.layout.store,list);
+//        list_item.setAdapter(adapter);
+    }
 
-        list_item=findViewById(R.id.list_item);
-        mylistadapter adapter= new mylistadapter(this,R.layout.store,list);
-        list_item.setAdapter(adapter);
+    @Override
+    public void onItemClick(int position) {
+        Toast.makeText(this,"normal click at position:" +position,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onWhatEverClick(int position) {
+        Toast.makeText(this,"whatever click at position:" +position,Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onDeleteClick(int position) {
+        startActivity(new Intent(MainActivity.this,ordering.class));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDatabaseRef.removeEventListener(mDRListener);
     }
     public void ClickMenu(View view){
         //open drawe
